eb600f9a47935db407a7948820970a7c2483cf68
小七 <yss2021@qq.com>
2020-08-12 20:42:32 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+//导入全局样式
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+//导入axios
+import axios from 'axios'
+//配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
+//把axios挂载到vue的原型上使每一个组件都用this使用
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
